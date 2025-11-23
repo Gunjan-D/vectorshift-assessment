240a04fa98d248a3d08d9e5e9a931059195c4438
Gunjan Deshpande <gunjan.deshpande@stonybrook.edu>
2025-11-23 01:46:05 -0500

    Original assignment without HubSpot integration

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from integrations.airtable import authorize_airtable, get_items_airtable, oauth2callback_airtable, get_airtable_credentials
 from integrations.notion import authorize_notion, get_items_notion, oauth2callback_notion, get_notion_credentials
-from integrations.hubspot import authorize_hubspot, get_hubspot_credentials, get_items_hubspot, oauth2callback_hubspot
 
 app = FastAPI()
 
@@ -59,19 +58,4 @@ async def get_notion_credentials_integration(user_id: str = Form(...), org_id: s
 async def get_notion_items(credentials: str = Form(...)):
     return await get_items_notion(credentials)
 
-# HubSpot
-@app.post('/integrations/hubspot/authorize')
-async def authorize_hubspot_integration(user_id: str = Form(...), org_id: str = Form(...)):
-    return await authorize_hubspot(user_id, org_id)
 
-@app.get('/integrations/hubspot/oauth2callback')
-async def oauth2callback_hubspot_integration(request: Request):
-    return await oauth2callback_hubspot(request)
-
-@app.post('/integrations/hubspot/credentials')
-async def get_hubspot_credentials_integration(user_id: str = Form(...), org_id: str = Form(...)):
-    return await get_hubspot_credentials(user_id, org_id)
-
-@app.post('/integrations/hubspot/load')
-async def get_hubspot_items(credentials: str = Form(...)):
-    return await get_items_hubspot(credentials)
